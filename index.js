e35635c422c18b00470e864be26f8e234bded40b
Kestron06 <kestron@kestron.software>
2023-10-09 00:12:36 -0700

    Basics for setup

--- index.js
+++ index.js
@@ -1,9 +1,70 @@
 process.env=require("./env.json");
+const twilioClient = require('twilio')(process.env.twilioSid, process.env.twilioToken);
+function sendText(toWhom,what){
+    twilioClient.messages.create({
+        body: what.replaceAll("<","\\<"),
+        from: twilio.num,
+        to: toWhom
+    });
+}
+function makeCall(toWhom,what){
+    fs.writeFileSync("./static/msg.xml",`<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say voice="man">${what.replaceAll("<","\\<")}</Say>
+</Response>`);
+    twilioClient.calls.create({
+        url:process.env.hostedUrl+"/msg.xml",
+        method:"GET",
+        to:toWhom,
+        from:twilio.num
+    })
+}
 const {Client, ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCOmmandBuilder, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle}=require("discord.js");
 const fs=require("fs");
+const express=require("express");
+const path=require("path");
+const site=new express();
+site.listen(80);
+site.use(express.static(path.join(__dirname,"./static")));
+const storage=require("./storage.json");
+function save(){
+    fs.writeFileSync("./storage.json",JSON.stringify(storage));
+}
+
+const defaultGuild={
+
+};
+const defaultUser={
+
+};
 
 const client=new Client({intents:Object.keys(GatewayIntentBits).map((a)=>{
     return GatewayIntentBits[a]
 })});
+
+function notify(urgencyLevel,what){
+    switch(urgencyLevel){
+        default:
+        case 0:
+            client.users.cache.get(process.env.ownerId).send(what);//Notify Kestron06 directly
+        break;
+        case 1:
+            client.channels.cache.get(process.env.noticeChannel).send(what);//Notify the staff of the Kestron Support server
+        break;
+        case 2:
+            sendText(what);//Text Kestron06
+        break;
+        case 3:
+            makeCall(what);//Call Kestron06
+        break;
+    }
+}
+
+client.on("messageCreate",async msg=>{
+
+});
+client.on("interactionCreate",async cmd=>{
+
+});
 
 client.login(process.env.token);
