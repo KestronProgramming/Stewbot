4e9716c677b1f4c6fa9d8e1fe284448bd43c3879
Kestron06 <kestron@kestron.software>
2023-10-09 12:05:37 -0700

    Yet more basics

--- index.js
+++ index.js
@@ -30,12 +30,62 @@ const storage=require("./storage.json");
 function save(){
     fs.writeFileSync("./storage.json",JSON.stringify(storage));
 }
+function objCopy(obj){
+    return JSON.parse(JSON.stringify(obj));
+}
 
 const defaultGuild={
-
+    "filter":{
+        "blacklist":[],
+        "active":false,
+        "censor":true,
+        "logOffenses":"",
+        "whitelist":[]
+    },
+    "starboard":{
+        "channel":"",
+        "emoji":"⭐",
+        "active":false,
+        "threshold":3
+    },
+    "logs":{
+        "channel":"",
+        "active":"",
+        "events":{
+            "channel":false,
+            "user":false,
+            "server":false,
+            "pins":false,
+            "emojis":false,
+            "events":false,
+            "invites":false,
+            "roles":false
+        }
+    },
+    "plugins":{
+        "entertainment":true,
+        "social":true,
+        "informational":true,
+        "miis":true,
+        "goombaSquad":true,
+        "ai":true
+    },
+    "users":[]
+};
+const defaultGuildUser={
+    "offenses":0,
+    "stars":0
 };
 const defaultUser={
-
+    "offenses":0,
+    "goombaSquad":{
+        "wins":0,
+        "losses":0,
+        "draws":0,
+        "cards":[],
+        "chose":null,
+        "playing":null
+    }
 };
 
 const client=new Client({intents:Object.keys(GatewayIntentBits).map((a)=>{
@@ -58,7 +108,7 @@ function notify(urgencyLevel,what){
         break;
     }
 }
-const uptime=0;
+var uptime=0;
 client.once("ready",()=>{
     uptime=Math.round(Date.now()/1000);
     notify(0,`Started <t:${uptime}:R>`);
@@ -69,7 +119,20 @@ client.on("messageCreate",async msg=>{
 
 });
 client.on("interactionCreate",async cmd=>{
+    switch(cmd.commandName){
+        case 'ping':
+            cmd.reply(`**Online**\n- Latency: ${client.ws.ping} milliseconds\n- Last Started: <t:${uptime}:f>, <t:${uptime}:R>\n- Uptime: ${((Math.round(Date.now()/1000)-uptime)/(1000*60*60)).toFixed(2)} hours`);
+        break;
+    }
+});
 
+client.on("rateLimit",async d=>{
+    notify(1,"Ratelimited -\n\n"+d);
+});
+client.on("guildCreate",async guild=>{
+    storage[guild.id]=objCopy(defaultGuild);
+    notify(1,"Added to a new server! "+guild.name);
+    save();
 });
 
 client.login(process.env.token);
