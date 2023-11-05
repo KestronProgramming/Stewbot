efb681fceddfa861de63d34dafe0bf90d812de76
Kestron06 <kestron@kestron.software>
2023-11-05 01:09:48 -0700

    Some filter stuff

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function makeCall(toWhom,what){
         from:twilio.num
     })
 }
-const {Client, ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCOmmandBuilder, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle}=require("discord.js");
+const {Client, ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle, Partials}=require("discord.js");
 const fs=require("fs");
 const express=require("express");
 const path=require("path");
@@ -29,9 +29,6 @@ site.use(express.static(path.join(__dirname,"./static")));
 const storage=require("./storage.json");
 function save(){
     fs.writeFileSync("./storage.json",JSON.stringify(storage));
-}
-function objCopy(obj){
-    return JSON.parse(JSON.stringify(obj));
 }
 
 const defaultMii={
@@ -80,9 +77,6 @@ const defaultMii={
     },
     name: 'Default',
     creatorName: ''
-  };
-const defaultReactionRole={//Indexed under messageId
-    "roles":{}//{ emojiId:roleId }
 };
 const defaultInvite={//Indexed under inviteId
     "uses":0,
@@ -132,12 +126,12 @@ const defaultGuild={
         "legit":true, //If manually setting the next number, disqualify from the overarching leaderboard
         "reset":true
     },
-    "users":[],
+    "users":{},
     "reactionRoles":[],
     "invites":[]
 };
 const defaultGuildUser={
-    "offenses":0,
+    "infractions":0,
     "stars":0,
     "invited":0
 };
@@ -151,12 +145,13 @@ const defaultUser={
         "chose":null,
         "playing":null
     },
-    "mii":objCopy(defaultMii)
+    "mii":structuredClone(defaultMii)
 };
 
-const client=new Client({intents:Object.keys(GatewayIntentBits).map((a)=>{
-    return GatewayIntentBits[a]
-})});
+const client=new Client({
+    intents:Object.keys(GatewayIntentBits).map(a=>{return GatewayIntentBits[a]}),
+    partials:Object.keys(Partials).map(a=>{return Partials[a]})
+});
 function notify(urgencyLevel,what){
     switch(urgencyLevel){
         default:
@@ -182,12 +177,101 @@ client.once("ready",()=>{
     save();
 });
 client.on("messageCreate",async msg=>{
-
+    if(msg.author.id===client.user.id) return;
+    msg.guildId=msg.guildId||"0";
+    if(msg.guildId!=="0"){
+        if(!storage.hasOwnProperty(msg.guildId)){
+            storage[msg.guildId]=structuredClone(defaultGuild);
+            save();
+        }
+        if(!storage[msg.guildId].users.hasOwnProperty(msg.author.id)){
+            storage[msg.guildId].users[msg.author.id]=structuredClone(defaultGuildUser);
+            save();
+        }
+    }
+    if(!storage.hasOwnProperty(msg.author.id)){
+        storage[msg.author.id]=structuredClone(defaultUser);
+        save();
+    }
+    if(storage[msg.guildId].filter.active===true){
+        console.log(storage[msg.guildId].filter.active);
+        var foundOne=false;
+        storage[msg.guildId].filter.blacklist.forEach(blockedWord=>{
+            if(new RegExp(`([^\\D]|\\b)${blockedWord}(ing|s|ed|er|ism|ist)*([^\\D]|\\b)`,"ig").test(msg.content)){
+                foundOne=true;
+                msg.content.replace(new RegExp(`([^\\D]|\\b)${blockedWord}(ing|s|ed|er|ism|ist)*([^\\D]|\\b)`,"ig"),"[\_]");
+            }
+        });
+        if(foundOne){
+            storage[msg.guildId].users[msg.author.id].infractions++;
+            msg.delete();
+            msg.channel.send(`\`\`\`\nI have censored a message from ${msg.user.username}.\`\`\`\n\n${msg.content}`);
+            return;
+        }
+    }
 });
 client.on("interactionCreate",async cmd=>{
+    try{
+        if(cmd.guild.id!==0){
+            if(!cmd.guild.id in storage){
+                storage[cmd.guild.id]=structuredClone(defaultGuild);
+                save();
+            }
+            if(!cmd.user.id in storage[cmd.guild.id].users){
+                storage[cmd.guild.id].users[cmd.user.id]=structuredClone(defaultGuildUser);
+                save();
+            }
+        }
+    }
+    catch(e){
+        cmd.guild={"id":"0"};
+    }
+    if(!cmd.user.id in storage){
+        storage[cmd.user.id]=structuredClone(defaultUser);
+        save();
+    }
+    //Slash Commands
     switch(cmd.commandName){
         case 'ping':
             cmd.reply(`**Online**\n- Latency: ${client.ws.ping} milliseconds\n- Last Started: <t:${uptime}:f>, <t:${uptime}:R>\n- Uptime: ${((Math.round(Date.now()/1000)-uptime)/(1000*60*60)).toFixed(2)} hours`);
+        break;
+        case 'no_swear':
+            if(storage[cmd.guild.id].filter.blacklist.includes(cmd.options.getString("word"))){
+                cmd.reply({"ephemeral":true,"content":`The word ||${cmd.options.getString("word")}|| is already in the blacklist.${storage[cmd.guild.id].filter.active?"":"To begin filtering in this server, use `/filter_config`."}`});
+            }
+            else{
+                storage[cmd.guild.id].filter.blacklist.push(cmd.options.getString("word"));
+                cmd.reply(`Added ||${cmd.options.getString("word")}|| to the filter for this server.${storage[cmd.guild.id].filter.active?"":`\n\nThe filter for this server is currently disabled. To enable it, use \`/filter_config\`.`}`);
+                save();
+            }
+        break;
+        case 're_swear':
+            if(storage[cmd.guild.id].filter.blacklist.includes(cmd.options.getString("word"))){
+                storage[cmd.guild.id].filter.blacklist.splice(storage[cmd.guild.id].filter.blacklist.indexOf(cmd.options.getString("word")),1);
+                cmd.reply(`Alright, I have removed ||${cmd.options.getString("word")}|| from the filter.`);
+                save();
+            }
+            else{
+                cmd.reply(`I'm sorry, but I don't appear to have that word in my blacklist. Are you sure you're spelling it right? You can use \`/view_filter\` to see all filtered words.`);
+            }
+        break;
+        case 'view_filter':
+            if(storage[cmd.guild.id].filter.blacklist.length>0){
+                cmd.reply({"content":`**Warning!** There is no guarantee what kinds of words may be in the blacklist. There is a chance it could be heavily dirty. To continue, press the button below.`,"components":[new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId('view_filter').setLabel('DM me the blacklist').setStyle(ButtonStyle.Danger))]});
+            }
+            else{
+                cmd.reply(`This server doesn't have any words blacklisted at the moment. To add some, you can use \`/no_swear\`.`);
+            }
+        break;
+    }
+    //Buttons
+    switch(cmd.customId){
+        case "view_filter":
+            cmd.user.send({"content":`The following is the blacklist for **${cmd.guild.name}** as requested.\n\n||${storage[cmd.guild.id].filter.blacklist.join("||, ||")}||`,"components":[new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId("delete-all").setLabel("Delete message").setStyle(ButtonStyle.Success))]});
+            cmd.deferUpdate();
+        break;
+        case "delete-all":
+            cmd.message.delete();
         break;
     }
 });
@@ -196,7 +280,7 @@ client.on("rateLimit",async d=>{
     notify(1,"Ratelimited -\n\n"+d);
 });
 client.on("guildCreate",async guild=>{
-    storage[guild.id]=objCopy(defaultGuild);
+    storage[guild.id]=structuredClone(defaultGuild);
     notify(1,"Added to a new server! "+guild.name);
     save();
 });
--- launchCommands.js
+++ launchCommands.js
@@ -1,9 +1,16 @@
 process.env=require("./env.json");
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST,Routes} = require('discord.js');
+const { REST,Routes,PermissionFlagsBits,SlashCommandBuilder} = require('discord.js');
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Check uptime stats')
+	new SlashCommandBuilder().setName('ping').setDescription('Check uptime stats'),
+	new SlashCommandBuilder().setName('no_swear').setDescription('Add a word to the filter').addStringOption(option=>
+		option.setName("word").setDescription("The word to blacklist").setRequired(true)
+	)
+	.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages).setDMPermission(false),
+	new SlashCommandBuilder().setName('re_swear').setDescription('Remove a word from the filter').addStringOption(option=>
+		option.setName("word").setDescription("The word to remove from the blacklist").setRequired(true)
+	).setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages).setDMPermission(false),
+	new SlashCommandBuilder().setName('view_filter').setDescription('View all blacklisted words for this server').setDMPermission(false)
 ]
 	.map(command => command.toJSON());
 
