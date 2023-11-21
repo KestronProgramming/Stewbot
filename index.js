7fee4f64e9c0b5e01c5761a64292a6ef18917939
Kestron06 <kestron@kestron.software>
2023-11-20 23:41:35 -0800

    After much work, tickets

--- index.js
+++ index.js
@@ -420,7 +420,7 @@ client.on("messageCreate",async msg=>{
     }
 
     var links=msg.content.match(discordMessageRegex)||[];
-    if(!storage[msg.author.id].config.embedPreviews||!storage[msg.guild.id].config.embedPreviews){
+    if(!storage[msg.author.id].config.embedPreviews||!storage[msg.guildId]?.config.embedPreviews){
         links=[];
     }
     var embs=[];
@@ -462,6 +462,69 @@ client.on("messageCreate",async msg=>{
         }
     }
     if(embs.length>0) msg.reply({content:`Embedded linked message${embs.length>1?"s":""}. You can prevent this behavior by surrounding message links in \`<\` and \`>\`.`,embeds:embs,files:fils});
+
+    if(msg.channel.name?.startsWith("Ticket with ")&&!msg.author.bot){
+        var resp={files:[],content:`Ticket response from **${msg.guild.name}**. To respond, make sure to reply to this message.\nTicket ID: ${msg.channel.name.split("Ticket with ")[1].split(" in ")[1]}/${msg.channel.id}`};
+        msg.attachments.forEach((attached,i) => {
+            let url=attached.proxyURL.toLowerCase();
+            if(i!==0||(!url.includes(".jpg")&&!url.includes(".png")&&!url.includes(".jpeg"))){
+                resp.files.push(attached.proxyURL);
+            }
+        });
+        resp.embeds=[new EmbedBuilder()
+            .setColor(0x006400)
+            .setTitle(`Ticket Message from ${msg.guild.name}`)
+            .setAuthor({
+                name: msg.author.globalName||msg.author.username,
+                iconURL:msg.author.displayAvatarURL(),
+                url:`https://discord.com/users/${msg.author.id}`
+            })
+            .setDescription(msg.content?msg.content:"⠀")
+            .setTimestamp(new Date(msg.createdTimestamp))
+            .setThumbnail(msg.guild.iconURL())
+            .setFooter({
+                text: "Make sure to reply to this message to respond",
+            })
+            .setImage(msg.attachments.first()?msg.attachments.first().proxyURL:null)
+        ];
+        client.users.cache.get(msg.channel.name.split("Ticket with ")[1].split(" in ")[0]).send(resp);
+    }
+    if(msg.reference&&msg.channel instanceof DMChannel){
+        var rmsg=await msg.channel.messages.fetch(msg.reference.messageId);
+        if(rmsg.author.id===client.user.id&&rmsg.content.includes("Ticket ID: ")){
+            var resp={
+                content:msg.content,
+                username:msg.author.nickname||msg.author.globalName||msg.author.username,
+                avatar_url:msg.author.displayAvatarURL()
+            };
+            var hook=await client.channels.cache.get(rmsg.content.split("Ticket ID: ")[1].split("/")[0]).fetchWebhooks();
+            hook=hook.find(h=>h.token);
+            if(hook){
+                fetch(`https://discord.com/api/webhooks/${hook.id}/${hook.token}?thread_id=${rmsg.content.split("Ticket ID:")[1].split("/")[1]}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(resp),
+                }).then(d=>d.text());
+            }
+            else{
+                client.channels.cache.get(rmsg.content.split("Ticket ID: ")[1].split("/")[0]).createWebhook({
+                    name:"Stewbot",
+                    avatar: "https://cdn.discordapp.com/attachments/1145432570104926234/1170273261704196127/kt.jpg",
+                }).then(d=>{
+                    fetch(`https://discord.com/api/webhooks/${d.id}/${d.token}?thread_id=${rmsg.content.split("Ticket ID:")[1].split("/")[1]}`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(resp),
+                    });
+                });
+            }
+            return;
+        }
+    }
 
     if (msg.channel instanceof DMChannel&&!msg.author.bot) {
         sendMessage(msg, true);
@@ -722,6 +785,22 @@ client.on("interactionCreate",async cmd=>{
                 cmd.reply(`Attempted to translate${t.text!==cmd.options.getString("what")?`: \`${t.text}\`. If this is incorrect, try using ${cmds.translate} again and specify more.`:`, but I was unable to. Try using ${cmds.translate} again and specify more.`}`);
             });
         break;
+        case 'ticket':
+            cmd.reply({embeds:[{
+                "type": "rich",
+                "title": `${cmd.guild.name} Moderator Tickets`,
+                "description": `Press the button below to open up a private ticket with ${cmd.guild.name} moderators.`,
+                "color": 0x006400,
+                "thumbnail": {
+                    "url": cmd.guild.iconURL(),
+                    "height": 0,
+                    "width": 0
+                },
+                "footer": {
+                    "text": `Tickets will take place over DMs, make sure to have DMs open to ${client.user.username}.`
+                }
+            }],components:[new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId(`ticket-${cmd.options.getChannel("channel").id}`).setLabel("Create private ticket with staff").setStyle(ButtonStyle.Success))]});
+        break;
 
         //Context Menu Commands
         case 'delete_message':
@@ -923,7 +1002,9 @@ client.on("interactionCreate",async cmd=>{
             var hook=await client.channels.cache.get(cmd.values[0]).fetchWebhooks();
             hook=hook.find(h=>h.token);
             if(hook){
-                hook.send(resp);
+                hook.send(resp).then(()=>{
+                    msg.delete();
+                });
             }
             else{
                 client.channels.cache.get(cmd.values[0]).createWebhook({
@@ -1022,6 +1103,18 @@ client.on("interactionCreate",async cmd=>{
         else{
             cmd.member.roles.remove(role);
         }
+        cmd.deferUpdate();
+    }
+    if(cmd.customId?.startsWith("ticket-")){
+        client.channels.cache.get(cmd.customId.split("-")[1]).send(`Ticket opened by **${cmd.member.nickname||cmd.user.globalName||cmd.user.username}**.`).then(msg=>{
+            msg.startThread({
+                name:`Ticket with ${cmd.user.id} in ${cmd.customId.split("-")[1]}`,
+                autoArchiveDuration:60,
+                type:"GUILD_PUBLIC_THREAD",
+                reason:`Ticket opened by ${cmd.user.username}`
+            });
+            cmd.user.send(`Ticket opened in **${cmd.guild.name}**. You can reply to this message to converse in the ticket. Note that any messages not a reply will not be sent to the ticket.\n\nTicket ID: ${cmd.customId.split("-")[1]}/${msg.id}`);
+        });
         cmd.deferUpdate();
     }
 });
