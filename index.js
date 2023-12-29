7139c52121ffbe6fd4d8742057e93efa46559122
Kestron <kestron@kestron.software>
2023-12-29 14:21:20 -0800

    Use WKOA's functions to provide Bible autocorrect

--- index.js
+++ index.js
@@ -11,6 +11,112 @@ Object.keys(bible).forEach(book=>{
     properNames[book.toLowerCase()]=book;
     Bible[book.toLowerCase()]=bible[book];//Make everything lowercase for compatibility with sanitizing user input
 });
+const ignoreSize = 10 + Object.keys(Bible).reduce((a, b) => a.length > b.length ? a : b).length;
+const threshold = 3;
+function levenshtein(s, t) {
+    if (s === t) {
+        return 0;
+    }
+    var n = s.length, m = t.length;
+    if (n === 0 || m === 0) {
+        return n + m;
+    }
+    var x = 0, y, a, b, c, d, g, h, k;
+    var p = new Array(n);
+    for (y = 0; y < n;) {
+        p[y] = ++y;
+    }
+    for (; (x + 3) < m; x += 4) {
+        var e1 = t.charCodeAt(x);
+        var e2 = t.charCodeAt(x + 1);
+        var e3 = t.charCodeAt(x + 2);
+        var e4 = t.charCodeAt(x + 3);
+        c = x;
+        b = x + 1;
+        d = x + 2;
+        g = x + 3;
+        h = x + 4;
+        for (y = 0; y < n; y++) {
+            k = s.charCodeAt(y);
+            a = p[y];
+            if (a < c || b < c) {
+                c = (a > b ? b + 1 : a + 1);
+            }
+            else {
+                if (e1 !== k) {
+                    c++;
+                }
+            }
+            if (c < b || d < b) {
+                b = (c > d ? d + 1 : c + 1);
+            }
+            else {
+                if (e2 !== k) {
+                    b++;
+                }
+            }
+            if (b < d || g < d) {
+                d = (b > g ? g + 1 : b + 1);
+            }
+            else {
+                if (e3 !== k) {
+                    d++;
+                }
+            }
+            if (d < g || h < g) {
+                g = (d > h ? h + 1 : d + 1);
+            }
+            else {
+                if (e4 !== k) {
+                    g++;
+                }
+            }
+            p[y] = h = g;
+            g = d;
+            d = b;
+            b = c;
+            c = a;
+        }
+    }
+    for (; x < m;) {
+        var e = t.charCodeAt(x);
+        c = x;
+        d = ++x;
+        for (y = 0; y < n; y++) {
+            a = p[y];
+            if (a < c || d < c) {
+                d = (a > d ? d + 1 : a + 1);
+            }
+            else {
+                if (e !== s.charCodeAt(y)) {
+                    d = c + 1;
+                }
+                else {
+                    d = c;
+                }
+            }
+            p[y] = d;
+            c = a;
+        }
+        h = d;
+    }
+    return h;
+}
+function getClosest(input) {
+    if (Object.keys(Bible).includes(input)) return input;
+    if (input.length > ignoreSize) return null;
+
+    var best = [null, Infinity]; // [bestOption, bestThreshold]
+    for (option of Object.keys(Bible)) {
+        const editsNeeded = levenshtein(option, input)
+        if (editsNeeded < best[1]) {
+            best = [option, editsNeeded]
+        }
+    }
+
+    if (best[1] < threshold) return best[0];
+    else return null;
+}
 const fs=require("fs");
 var storage=require("./storage.json");
 const cmds=require("./commands.json");
@@ -1384,7 +1490,7 @@ client.on("interactionCreate",async cmd=>{
             cmd.followUp("Configured your personal setup");
         break;
         case 'bible':
-            let book=cmd.options.getString("book").toLowerCase();
+            let book=getClosest(cmd.options.getString("book").toLowerCase());
             if(cmd.options.getString("verse").includes("-")&&+cmd.options.getString("verse").split("-")[1]>+cmd.options.getString("verse")[0]){
                 try{
                     let verses=[];
@@ -1424,11 +1530,11 @@ client.on("interactionCreate",async cmd=>{
                         }]});
                     }
                     else{
-                        cmd.followUp(`I'm sorry, I couldn't find \`${properNames[book]} ${cmd.options.getInteger("chapter")}:${cmd.options.getString("verse")}\`. Are you sure it exists? Perhaps something is typoed.`);
+                        cmd.followUp(`I'm sorry, I couldn't find \`${book} ${cmd.options.getInteger("chapter")}:${cmd.options.getString("verse")}\`. Are you sure it exists? Perhaps something is typoed.`);
                     }
                 }
                 catch(e){
-                    cmd.followUp(`I'm sorry, I couldn't find \`${properNames[book]} ${cmd.options.getInteger("chapter")}:${cmd.options.getString("verse")}\`. Are you sure it exists? Perhaps something is typoed.`);
+                    cmd.followUp(`I'm sorry, I couldn't find \`${book} ${cmd.options.getInteger("chapter")}:${cmd.options.getString("verse")}\`. Are you sure it exists? Perhaps something is typoed.`);
                 }
             }
         break;
