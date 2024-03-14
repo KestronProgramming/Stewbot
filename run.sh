6731a2f12ae262c0409282bc6d1e763e9a5e2392
WKOAPi <kestron@kestron.software>
2024-03-14 20:00:37 +0000

    Fix line endings and modify node path in run.sh - WKoA

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 while true; do
   # Attempt to run bot. Send all errors to a discord logger
-  node index.js 2>&1 | ./stderrLog.js
+  /usr/local/bin/node index.js 2>&1 | ./stderrLog.js
 
   # Sleep to avoid spamming during a bootloop
   sleep 1
