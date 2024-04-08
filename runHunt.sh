ded1dec90de273bcb0cff65d2582f3f662c26f8c
Kestron <kestron@kestron.software>
2024-04-07 23:09:43 -0700

    Delete runHunt.sh

--- runHunt.sh
+++
@@ -1,9 +0,0 @@
-# /bin/sh
-
-while true; do
-  # Attempt to run bot. Send all errors to a discord logger
-  /usr/local/bin/node hunt.js 2>&1 | ./stderrLog.js
-
-  # Sleep to avoid spamming during a bootloop
-  sleep 1
-done
