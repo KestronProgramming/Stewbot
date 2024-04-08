aa6c8c8698e7b74d42ae79803acd06ac9738add1
kestron06 <kestron@kestron.software>
2024-04-07 23:14:20 -0700

    I'm going B.O.N.K.E.R.S

--- index.js
+++ index.js
@@ -1028,7 +1028,7 @@ client.on("messageCreate",async msg=>{
                 sendHook({
                     username:msg.member?.nickname||msg.author.globalName||msg.author.username,
                     avatarURL:msg.member?.displayAvatarURL(),
-                    content:ll(`\`\`\`\nThe following message from ${msg.author.username} has been censored by Stewbot.\`\`\`${replyBlip}${msg.content}`)
+                    content:ll(`\`\`\`\nThe following message from ${msg.author.username} has been censored by Stewbot.\`\`\`${replyBlip}${msg.content.slice(0,1800).replace(/(https?\:\/\/|\n|\<?\@(\d+|everyone)\>?)/ig,"")}`)
                 });
             }
             if(storage[msg.author.id].config.dmOffenses&&msg.webhookId===null){
