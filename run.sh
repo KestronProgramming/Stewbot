bd5d2fb18be88ac55580aa1df52b5a889dbd1e90
WKOAPi <kestron@kestron.software>
2024-04-08 17:27:45 +0100

    Improvements to stderr log and whatever else kestron didn't commit yet, ignoring test.json because I have no clue what it is - WKoA

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # /bin/sh
 
 while true; do
-  git pull
+  # git pull # don't git pull atm, since we might have updated from pi
   # Attempt to run bot. Send all errors to a discord logger
   /usr/local/bin/node index.js 2>&1 | ./stderrLog.js
 
