86850c24310410a77cd49e0bba4d86c9a4f31e5e
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2024-09-30 13:46:20 -0500

    Patch /timeout bug

--- commands/timeout.js
+++ commands/timeout.js
@@ -46,6 +46,7 @@ module.exports = {
 		}
 		if(cmd.user.id===cmd.options.getUser("target").id){
 			cmd.followUp(`I cannot timeout you as the one invoking the command. If you feel the need to timeout yourself, consider changing your actions and mindset instead.`);
+			return;
 		}
 		var time=(cmd.options.getInteger("hours")*60000*60)+(cmd.options.getInteger("minutes")*60000)+(cmd.options.getInteger("seconds")*1000);
 		cmd.guild.members.cache.get(cmd.options.getUser("target").id).timeout(time>0?time:60000,`Instructed to timeout by ${cmd.user.username}: ${cmd.options.getString("reason")}`);
