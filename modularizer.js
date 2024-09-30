5f701a0b2fa53f08dcdb2394cbdf6dd9a9812b18
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2024-09-30 12:56:47 -0500

    Modularize /timestamp

--- modularizer.js
+++ modularizer.js
@@ -2,7 +2,7 @@
 
 const fs = require("fs");
 
-const command = "embed_message";
+const command = "timestamp";
 
 const code = fs.readFileSync("./index.js").toString()
 const launchCommands = fs.readFileSync("./launchCommands.js").toString()
