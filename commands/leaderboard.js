505da6021a68ff0495755e449be164067563970f
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2024-10-02 20:21:23 -0500

    Patch /leaderboard requiring global

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -30,7 +30,7 @@ module.exports = {
 
         extra: { "contexts": [0, 1, 2], "integration_types": [0, 1] },
 
-        requiredGlobals: ["cmds", "getLvl"],
+        requiredGlobals: ["cmds", "getLvl", "getEmojiFromMessage"],
 
         help: {
             "helpCategory": "Entertainment", 
