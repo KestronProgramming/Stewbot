5d95a7030d786c5b29ed2f7ca530d79a5d84672b
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2024-10-19 13:02:59 -0500

    Make /warnings code readable, warnings sortable

--- commands/warnings.js
+++ commands/warnings.js
@@ -15,6 +15,11 @@ module.exports = {
                 option.setName("who").setDescription("Do you want to see the warnings for a specific person?")
             ).addBooleanOption(option=>
                 option.setName("private").setDescription("Make the response ephemeral?").setRequired(false)
+            ).addStringOption(option =>
+                option.setName("sort").setDescription("How to sort the warnings").setChoices(
+                    { name: "By number of warnings", value: "warnings" },
+                    { name: "By sum of severity", value: "severity" },
+                ).setRequired(false)
             ).setDefaultMemberPermissions(PermissionFlagsBits.ManageNicknames),
 		
 		// Optional fields
@@ -32,6 +37,8 @@ module.exports = {
 
 	async execute(cmd, context) {
 		applyContext(context);
+
+        const sortMethod = cmd.options.getString("sort") || "severity";
 		
         if(cmd.options.getUser("who")!==null) {
             if(!storage[cmd.guild.id].users.hasOwnProperty(cmd.options.getUser("who").id)){
@@ -43,34 +50,43 @@ module.exports = {
             cmd.followUp({content:limitLength(`${storage[cmd.guild.id].users[cmd.options.getUser("who").id].warnings.length>0?`There are ${storage[cmd.guild.id].users[cmd.options.getUser("who").id].warnings.length} warnings for <@${cmd.options.getUser("who").id}>.${storage[cmd.guild.id].users[cmd.options.getUser("who").id].warnings.map((a,i)=>`\n${i}. <@${a.moderator}>: \`${a.reason}\`, level **${a.severity}**, given <t:${a.when}:R>.`).join("")}`:`This user has no warnings currently present.`}`),allowedMentions:{parse:[]},components:[new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Remove a Warning").setStyle(ButtonStyle.Danger).setCustomId(`remWarn-${cmd.options.getUser("who").id}`),new ButtonBuilder().setLabel("Clear all Warnings").setStyle(ButtonStyle.Danger).setCustomId(`clearWarn-${cmd.options.getUser("who").id}`))]});
         }
         else {
+            // Collect all users with warnings
+            const usersWithWarnings = Object.keys(storage[cmd.guild.id].users).filter(id => {
+                const user = storage[cmd.guild.id].users[id];
+                return user.hasOwnProperty("warnings") && user.warnings.length > 0;
+            })
 
-			const warningsArray = Object.keys(storage[cmd.guild.id].users).map(
-                (a) =>
-                    storage[cmd.guild.id].users[a].hasOwnProperty("warnings")
-                        ? storage[cmd.guild.id].users[a].warnings.length > 0
-                            ? `\n- <@${a}>: \`${
-                                  storage[cmd.guild.id].users[a].warnings.length
-                              }\` warnings dealt. Sum of Severities: \`${
-                                  storage[cmd.guild.id].users[a].warnings
-                                      .length > 1
-                                      ? storage[cmd.guild.id].users[
-                                            a
-                                        ].warnings.reduce(
-                                            (b, c) =>
-                                                (typeof b === "object"
-                                                    ? b.severity
-                                                    : b) + c.severity
-                                        )
-                                      : storage[cmd.guild.id].users[a]
-                                            .warnings[0].severity
-                              }\``
-                            : ``
-                        : ``
-            ).filter(warning => warning !== "");
+            // Collect data we need from these user
+            const warningData = usersWithWarnings.map(id => {
+                const usersWarnings = storage[cmd.guild.id].users[id].warnings;
+                return [ // I'd use json but arrays are faster and we could be searching through 250K users
+                    id,
+                    usersWarnings.length,
+                    usersWarnings.reduce((b, c) =>
+                        (typeof b === "object" ? b.severity : b) + c.severity
+                    , {severity:0}),
+                ]
+            })
+
+            // Sort  <-  this is a code comment
+            warningData.sort((data1, data2) => {
+                switch (sortMethod) {
+                    case "warnings":
+                        return data2[1] - data1[1];
+                    case "severity":
+                        return data1[1] - data2[1];
+                }
+            })
+            console.log(warningData)
+
+            // Parse data into warnings message
+            const warningsMessage = warningData.map( data => {
+                return `\n- <@${data[0]}>: \`${data[1]}\` warnings dealt. Sum of Severities: \`${data[2]}\``
+            })
 
             cmd.followUp({
                 content: limitLength(
-                    `**Warnings in ${cmd.guild.name}**${warningsArray.length > 0 ? warningsArray.join("") : `\n-# No Warnings have been issued. Use ${cmds.warn.mention} to issue a warning.`}`
+                    `**Warnings in ${cmd.guild.name}**${warningsMessage.length > 0 ? warningsMessage.join("") : `\n-# No Warnings have been issued. Use ${cmds.warn.mention} to issue a warning.`}`
                 ),
                 allowedMentions: { parse: [] },
             });
