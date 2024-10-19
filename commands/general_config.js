a0084d36af4f73d605b6de95afb1748d817f96b3
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2024-10-18 19:26:16 -0500

    /general_config persona->server

--- commands/general_config.js
+++ commands/general_config.js
@@ -36,6 +36,6 @@ module.exports = {
 		
 		if(cmd.options.getBoolean("embeds")!==null) storage[cmd.guildId].config.embedPreviews=cmd.options.getBoolean("embeds");
 		if(cmd.options.getBoolean("disable_anti_hack")!==null) storage[cmd.guildId].disableAntiHack=cmd.options.getBoolean("disable_anti_hack");
-		cmd.followUp("Configured your personal setup");
+		cmd.followUp("Configured your server setup");
 	}
 };
