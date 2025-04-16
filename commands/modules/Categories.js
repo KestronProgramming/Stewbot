223a62bcefd5b8064a9740ae3b28b4353b61ec6d
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-04-16 17:46:04 -0500

    Fix Categories intellisence

--- commands/modules/Categories.js
+++ commands/modules/Categories.js
@@ -1,32 +1,41 @@
-const Categories = [
-    "General",              // Generic commands almost every bot has
-    "Bot",                  // A command designed specifically for managing the bot itself
-    "Information",          // A command designed purely to provide information of some kind
-    "Entertainment",        // A command that is related to a fun feature of some kind
-    "Configuration",        // A command that changes settings of some kind
-    "Administration",       // A command that needs moderator privileges
-    "Safety",               // Anti-hack, anti-spam, etc
-    "Context_Menu",         // A command accessed via the context menu
-    "Server_Only",          // Commands that can only be run in servers
-    "Module",               // Any plugin that sits the background and doesn't have a slash command (or only has a single command to configure it?).
-]
-
-/** 
- * Enum for command categories
+/**
+ * Command categories for the bot
  * @enum {string}
  * @readonly
- * @property {string} General - Generic commands almost every bot has
- * @property {string} Bot - A command designed specifically for managing the bot itself
- * @property {string} Information - A command designed purely to provide information of some kind
- * @property {string} Entertainment - A command that is related to a fun feature of some kind
- * @property {string} Configuration - A command that changes settings of some kind
- * @property {string} Administration - A command that needs moderator privileges
- * @property {string} Safety - Anti-hack, anti-spam, etc
- * @property {string} Context_Menu - A command accessed via the context menu
- * @property {string} Server_Only - Commands that can only be run in servers
- * @property {string} Module - Any plugin that sits the background and doesn't have a slash command (or only has a single command to configure it?).
  */
-const CategoriesEnum = Object.fromEntries(Categories.map(cat => [cat, cat]))
-
-// Export as an enum
-module.exports = CategoriesEnum
+const Categories = {
+    /** Generic commands almost every bot has */
+    General: "General",
+    
+    /** A command designed specifically for managing the bot itself */
+    Bot: "Bot",
+    
+    /** A command designed purely to provide information of some kind */
+    Information: "Information",
+    
+    /** A command that is related to a fun feature of some kind */
+    Entertainment: "Entertainment",
+    
+    /** A command that changes settings of some kind */
+    Configuration: "Configuration",
+    
+    /** A command that needs moderator privileges */
+    Administration: "Administration",
+    
+    /** Anti-hack, anti-spam, etc */
+    Safety: "Safety",
+    
+    /** A command accessed via the context menu */
+    Context_Menu: "Context_Menu",
+    
+    /** Commands that can only be run in servers */
+    Server_Only: "Server_Only",
+    
+    /** Any plugin that sits the background and doesn't have a slash command */
+    Module: "Module"
+  };
+  
+  // Freeze the object to make it truly const
+  Object.freeze(Categories);
+  
+  module.exports = Categories;
