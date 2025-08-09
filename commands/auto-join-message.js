97aa7a000d38e5620aafddf6fcd9a2d4c86cc420
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-08-09 17:14:15 -0500

    some auto join message QoL

--- commands/auto-join-message.js
+++ commands/auto-join-message.js
@@ -1,9 +1,9 @@
 // #region CommandBoilerplate
 const Categories = require("./modules/Categories");
 const client = require("../client.js");
-const { Guilds, Users, guildByID, userByID, guildByObj, userByObj } = require("./modules/database.js")
-const { Events, ContextMenuCommandBuilder, InteractionContextType: IT, ApplicationIntegrationType: AT, ApplicationCommandType, SlashCommandBuilder, Client, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle, Partials, ActivityType, PermissionFlagsBits, DMChannel, RoleSelectMenuBuilder, ChannelSelectMenuBuilder, ChannelType,AuditLogEvent, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, MessageReaction, MessageType}=require("discord.js");
-function applyContext(context={}) {
+const { Guilds, Users, guildByID, userByID, guildByObj, userByObj } = require("./modules/database.js");
+const { Events, ContextMenuCommandBuilder, InteractionContextType: IT, ApplicationIntegrationType: AT, ApplicationCommandType, SlashCommandBuilder, Client, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle, Partials, ActivityType, PermissionFlagsBits, DMChannel, RoleSelectMenuBuilder, ChannelSelectMenuBuilder, ChannelType, AuditLogEvent, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, MessageReaction, MessageType } = require("discord.js");
+function applyContext(context = {}) {
 	for (let key in context) {
 		this[key] = context[key];
 	}
@@ -22,46 +22,46 @@ module.exports = {
 	data: {
 		// Slash command data
 		command: new SlashCommandBuilder().setName("auto-join-message").setDescription("Set up a message to be sent automatically when a user joins")
-			.addBooleanOption(option=>
+			.addBooleanOption(option =>
 				option.setName("active").setDescription("Should I send a message when the user joins?").setRequired(true)
-			).addStringOption(option=>
+			).addStringOption(option =>
 				option.setName("message").setDescription("The message to be sent (Use \"${@USER}\" to mention the user)")
-			).addStringOption(option=>
+			).addStringOption(option =>
 				option.setName("channel_or_dm").setDescription("Should I post this message in a channel or the user's DMs?").addChoices(
-					{"name":"Channel","value":"channel"},
-					{"name":"DM","value":"dm"}
+					{ "name": "Channel", "value": "channel" },
+					{ "name": "DM", "value": "dm" }
 				)
-			).addChannelOption(option=>
+			).addChannelOption(option =>
 				option.setName("channel").setDescription("The channel to post the message to").addChannelTypes(ChannelType.GuildText)
-			).addBooleanOption(option=>
+			).addBooleanOption(option =>
 				option.setName("private").setDescription("Make the response ephemeral?").setRequired(false)
 			).setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
-		
+
 		// Optional fields
-		
-		extra: {"contexts":[0],"integration_types":[0]},
+
+		extra: { "contexts": [0], "integration_types": [0] },
 
 		requiredGlobals: [],
 
 		help: {
 			helpCategories: [Categories.Administration, Categories.Configuration, Categories.Server_Only],
 			shortDesc: "Set up a message to be sent automatically when a user joins",
-			detailedDesc: 
+			detailedDesc:
 				`Configures a template message Stewbot will post to a designated channel or the user's DMs in the server's name every time a user joins`
 		},
 	},
 
-    /** @param {import('discord.js').ChatInputCommandInteraction} cmd */
-    async execute(cmd, context) {
+	/** @param {import('discord.js').ChatInputCommandInteraction} cmd */
+	async execute(cmd, context) {
 		applyContext(context);
 
 		const guild = await guildByObj(cmd.guild);
-		
+
 		guild.ajm.active = cmd.options.getBoolean("active");
 		if (cmd.options.getChannel("channel") !== null) guild.ajm.channel = cmd.options.getChannel("channel").id;
 		if (cmd.options.getString("channel_or_dm") !== null) guild.ajm.dm = cmd.options.getString("channel_or_dm") === "dm";
 		if (cmd.options.getString("message") !== null) guild.ajm.message = (await checkDirty(config.homeServer, cmd.options.getString("message"), true))[1];
-		
+
 		var disclaimers = [];
 		if (!guild.ajm.dm && guild.ajm.channel === "") {
 			guild.ajm.dm = true;
@@ -86,7 +86,7 @@ module.exports = {
 				readGuildStore.ajm.dm = true;
 
 				// Save it (guildStore is readonly here)
-				Guilds.updateOne({ id: readGuildStore.id }, { $set: { "ajm.dm": true } })
+				Guilds.updateOne({ id: readGuildStore.id }, { $set: { "ajm.dm": true } });
 			}
 
 			if (readGuildStore.ajm.dm) {
